4d6d094c8e99928067ec4d34c56c95d82f1a7ba7
Oscar Giraldo <softnewworld@gmail.com>
2016-08-05 11:36:55 -0500

    Configuración de certificado

---
+++ app/src/main/java/com/sisapp/sisapp/LoaderActivity.java
@@ -0,0 +1,17 @@
+package com.sisapp.sisapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Elmost on 4/08/2016.
+ */
+public class LoaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+    }
+}
